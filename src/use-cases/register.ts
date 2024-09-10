fff1747af5ff86aec7f667abba294237075e162a
FlavioPals <flaviopals@hotmail.com>
2024-09-10 11:52:04 -0300

    Caso de Uso e Design Patterns - Repository Pattern

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { hash } from "bcryptjs"
 import { prisma } from "../lib/prisma"
+import { PrismaUsersRepository } from "../repositories/prisma-users-repository"
 
 interface RegisterUseCaseRequest{
     name: string
@@ -8,20 +9,22 @@ interface RegisterUseCaseRequest{
 }
 
 export async function registerUseCase({name, email, password}: RegisterUseCaseRequest){
+    const password_hash = await hash(password, 2)
+
     const userWithSameEmail = await prisma.user.findUnique({
         where:{
             email,
         }
     })
+
     if(userWithSameEmail){
         throw new Error('User already exists')
         }
-    const password_hash = await hash(password, 2)
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password: password_hash
-        }
+    const prismaUsersRepository = new PrismaUsersRepository()
+    prismaUsersRepository.create({
+        name,
+        email,
+        password:password_hash
     })
+
 }
