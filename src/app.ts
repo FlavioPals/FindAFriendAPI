76cad42eaa614dae18466d51da1a83ca94ffefd4
FlavioPals <flaviopals@hotmail.com>
2024-09-11 09:15:18 -0300

    configurando o vitest

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,11 @@ export const app = fastify();
 
 app.register(appRoutes)
 
+app.setErrorHandler((error, _, reply) => {
+    if(error instanceof Error){
+        return reply.status(400).send({
+            message: error.message
+        })
+    }
+    return reply.status(500).send()
+})
--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
 
 import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository"
-import { RegisterUseCase } from "../../use-cases/register.use-case"
+import { RegisterUseCase } from "../../use-cases/register"
 import { UserAlreadyExistsError } from "../../errors/user-already-exists"
 
 export async function register(request:FastifyRequest, reply:FastifyReply){
@@ -22,7 +22,7 @@ export async function register(request:FastifyRequest, reply:FastifyReply){
             return reply.status(409).send({message: err.message})
         }
 
-        return reply.status(500).send()//TODO: fix me
+        throw err
     }
     return reply.status(201).send()
 }
