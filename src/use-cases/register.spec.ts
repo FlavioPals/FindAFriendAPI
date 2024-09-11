4bc558636fa02e963c14a5b6da69b84cdbc0ebe8
FlavioPals <flaviopals@hotmail.com>
2024-09-11 09:44:34 -0300

    criando teste unitario para register

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,56 @@
-import { expect, test } from "vitest";
+import { describe, expect, it, test } from "vitest";
+import { PrismaUsersRepository } from "../repositories/prisma/prisma-users-repository";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "../repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExistsError } from "../errors/user-already-exists";
 
-test('check if it works', () => {
-    expect(2+2).toBe(4)
+describe('Register Use Case', () => {
+
+    it('should be able to register', async () => {
+        const usersRepository = new InMemoryUsersRepository();
+        const sut = new RegisterUseCase(usersRepository);
+        
+        const {user} =  await sut.execute({
+            name: 'John Doe',
+            email: 'QpNkS@example.com',
+            password: '123456'
+        })
+        expect(user.id).toEqual(expect.any(String))
+    })
+
+    it('should hash user password upon registration', async () => {
+        const usersRepository = new InMemoryUsersRepository();
+        const sut = new RegisterUseCase(usersRepository);
+        const {user} =  await sut.execute({
+            name: 'John Doe',
+            email: 'QpNkS@example2.com',
+            password: '123456'
+        })
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password
+        )
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+        expect(user.password).not.toBe('123456')
+    })
+
+    it('should not be able to register with same email twice', async () => {
+        const usersRepository = new InMemoryUsersRepository();
+        const sut = new RegisterUseCase(usersRepository);
+
+        await sut.execute({
+            name: 'John Doe',
+            email: 'QpNkS@example.com',
+            password: '123456'
+        })
+        expect(() => 
+            sut.execute({
+                name: 'John Doe',
+                email: 'QpNkS@example.com',
+                password: '123456'
+            })
+        ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+    })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -3,25 +3,36 @@ import { prisma } from "../lib/prisma"
 import { PrismaUsersRepository } from "../repositories/prisma/prisma-users-repository"
 import { UsersRepository } from "../repositories/users-repository"
 import { UserAlreadyExistsError } from "../errors/user-already-exists"
+import { User } from "@prisma/client"
 
 interface RegisterUseCaseRequest{
     name: string
     email: string
     password: string
 }
+interface RegisterUseCaseResponse{
+    user:User
+}
+
 export class RegisterUseCase{
     constructor(private usersRepository: UsersRepository){}
-    async execute({name, email, password}: RegisterUseCaseRequest){
+
+    async execute({
+        name,
+         email,
+          password
+        }: RegisterUseCaseRequest):Promise<RegisterUseCaseResponse>{
         const password_hash = await hash(password, 2)
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
         if(userWithSameEmail){
             throw new UserAlreadyExistsError()
             }
-       
-        this.usersRepository.create({
+     const user = await this.usersRepository.create({
             name,
             email,
             password:password_hash
         })
+
+        return {user}
     }
 }
