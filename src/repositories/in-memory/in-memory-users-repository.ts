bd41e58f865235bdb1da5c4636c97c652c5acb99
FlavioPals <flaviopals@hotmail.com>
2024-09-11 09:45:02 -0300

    criando o repositorio do usuario na memoria para testes

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,25 @@
+import { Prisma, User } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+
+export class InMemoryUsersRepository implements UsersRepository{
+    public items: User[] = []
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        const user = {
+            id: 'user-1',
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            pets: data.pets,
+        }
+        this.items.push(user)
+
+        return user
+    }
+   async findByEmail(email: string): Promise<User | null> {
+       const user = this.items.find((item) => item.email === email)
+       if(!user){
+        return null
+       }
+       return user
+    }
+}
