b381e56028c356be896dad6cfe15b85c9cf722e9
FlavioPals <flaviopals@hotmail.com>
2024-09-16 14:50:37 -0300

    adicionando rotas de pets e orgs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import fastify from "fastify";
-import { appRoutes } from "./http/routes";
+import { orgRoutes } from "./http/controllers/org/routes";
+import fastifyJwt from "@fastify/jwt";
+import { petsRoutes } from "./http/controllers/pets/routes";
 
 
 
 export const app = fastify();
+app.register(fastifyJwt, {
+    secret: 'secretPassword'
+})
+app.get('/', () => {
+    return 'hello world'
+})
 
-app.register(appRoutes)
-
+app.register(orgRoutes)
+app.register(petsRoutes)
 app.setErrorHandler((error, _, reply) => {
     if(error instanceof Error){
         return reply.status(400).send({
